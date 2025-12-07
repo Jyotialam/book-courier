75f357d9b34304558ab44c11d271964ed75ae828
Jyoti Alam <jyotialam122@gmail.com>
2025-12-07 03:58:00 -0800

    added latest books section

---
+++ src/Components/LatestBooks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LatestBooks = () => {
+    return (
+        <div>
+           New books
+           view all button
+        </div>
+    );
+};
+
+export default LatestBooks;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner';
+import LatestBooks from '../../Components/LatestBooks';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <LatestBooks/>
         </div>
     );
 };
